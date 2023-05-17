0afa60bf1490c51459c68b77b3effda6f8222389
pedro823 <pedro.pereira@vtex.com.br>
2023-05-17 13:47:19 +0100

    Add redirect to znx

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/znx',
+        destination: 'https://open.spotify.com/track/3J5hqIWAC82uiLKUAuZuDd?si=9a7a948c686a49ab',
+        basePath: false,
+        permanent: true
+      }
+    ]
+  }
 }
