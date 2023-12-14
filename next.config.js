5b3c7bce24dd541ea0bb794a655d38e2899d1466
pedro823 <pedro.pereira@vtex.com.br>
2023-12-14 15:58:16 +0000

    Adds redirect to whispers

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ module.exports = {
         destination: 'https://open.spotify.com/track/3J5hqIWAC82uiLKUAuZuDd?si=9a7a948c686a49ab',
         basePath: false,
         permanent: true
+      },
+      {
+        source: '/whispers',
+        destination: 'https://open.spotify.com/track/0u4mBVSqA0ehUWgamYdtwp?si=d9b99467e0d4468d',
+        basePath: false,
+        permanent: true
       }
     ]
   }
